35ca12ff9772639f23bfe119aba6e6c298c176c9
Roman Senn <roman.l.senn@gmail.com>
2025-07-27 16:04:49 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { Map, View } from '../3rdparty/openlayers/src/ol/index.js';
 import { Tile as TileLayer } from '../3rdparty/openlayers/src/ol/layer.js';
 import { XYZ } from '../3rdparty/openlayers/src/ol/source.js';
-import { defaults as defaultControls, ScaleLine } from '../3rdparty/openlayers/src/ol/control.js';
-import { fromLonLat, addCoordinateTransforms, addProjection, transform } from '../3rdparty/openlayers/src/ol/proj.js';
+import {
+  defaults as defaultControls,
+  ScaleLine
+} from '../3rdparty/openlayers/src/ol/control.js';
+import {
+  fromLonLat,
+  addCoordinateTransforms,
+  addProjection,
+  transform
+} from '../3rdparty/openlayers/src/ol/proj.js';
 import trkl from '../lib/trkl.js';
 import { getset, gettersetter } from '../lib/misc.js';
 
@@ -19,11 +27,25 @@ const locations = Hash({
   glasbrunnen: LatLon(46.96186208844483, 7.411693012341456)
 });
 
-const backgroundLayer = new TileLayer({ id: 'background-layer', source: new XYZ({ url: `https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg` }) });
+const backgroundLayer = new TileLayer({
+  id: 'background-layer',
+  source: new XYZ({
+    url: `https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg`
+  })
+});
 
-const view = new View({ projection: 'EPSG:3857', center: locations.zuhause, zoom: 16 });
+const view = new View({
+  projection: 'EPSG:3857',
+  center: locations.zuhause,
+  zoom: 16
+});
 
-const map = new Map({ target: 'map', controls: defaultControls().extend([new ScaleLine({ units: 'metric' })]), layers: [backgroundLayer], view });
+const map = new Map({
+  target: 'map',
+  controls: defaultControls().extend([new ScaleLine({ units: 'metric' })]),
+  layers: [backgroundLayer],
+  view
+});
 
 Properties(globalThis, {
   zoom: [() => view.getZoom(), v => view.setZoom(v)],
@@ -33,9 +55,30 @@ Properties(globalThis, {
   center: [() => view.getCenter(), v => view.setCenter(v)]
 });
 
-const ol = { Map, View, TileLayer, XYZ, defaultControls, ScaleLine, fromLonLat, addCoordinateTransforms, addProjection, transform };
+const ol = {
+  Map,
+  View,
+  TileLayer,
+  XYZ,
+  defaultControls,
+  ScaleLine,
+  fromLonLat,
+  addCoordinateTransforms,
+  addProjection,
+  transform
+};
 
-Object.assign(globalThis, { ol, view, map, backgroundLayer, LatLon, locations, trkl, getset, gettersetter });
+Object.assign(globalThis, {
+  ol,
+  view,
+  map,
+  backgroundLayer,
+  LatLon,
+  locations,
+  trkl,
+  getset,
+  gettersetter
+});
 
 function Hash(obj) {
   return Object.setPrototypeOf(obj, null);
